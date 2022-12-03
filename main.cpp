82d62a29639c9e6f11c113d2755334be8ac1b435
josephckarch <karchjoseph@gmail.com>
2022-12-03 16:05:09 -0500

    added menu control

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
+int main()
+{
+    bool running = true;
+
+    // Initial messages
+    cout << "Hello! Welcome to COVID++\n";
+    int input = 0;
+
+    while(running) {
+        cout << "Please select an option to continue\n";
+        cout << "1. option1\n2. option2\n3. option3\n-1. Exit program\n";
+        cin >> input;
+
+        if (input == -1) { // exit
+            running = false;
+            break;
+        }
+    }
+
+
 }
